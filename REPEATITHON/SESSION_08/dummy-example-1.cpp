376d9a2083b6066dd96ecc92699ae5704dd69516
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-02-23 14:38:03 +0530

    session 08 repeatithon

---
+++ REPEATITHON/SESSION_08/dummy-example-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+class Test 
+{
+    int a; 
+    int b; 
+    int c; 
+    int d; 
+    int e; 
+    int f; 
+}; 
+
+int main(void) 
+{
+    Test t; 
+
+    t.a = 10; 
+    t.b = 20; 
+    t.c = 30; 
+    t.d = 40; 
+    t.e = 50; 
+    t.f = 60; 
+
+    return (0); 
+}
+
