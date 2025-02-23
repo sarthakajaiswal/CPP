8f9fa5390eebb0a3771c88b0f7320c4dfeda7bb3
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-02-23 14:34:43 +0530

    repeatithon session 06

---
+++ REPEATITHON/SESSION_06/Hello_World_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+
+int main(void) 
+{
+    std::cout << "Hello Repeatithon World!!!" << std::cout; 
+    return 0; 
+}
+
---
+++ REPEATITHON/SESSION_06/Hello_World_3.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+
+int main(void) 
+{
+    std::cout << "Hello Repeatithon World !!!" << std::endl; 
+    return 0; 
+}
+
