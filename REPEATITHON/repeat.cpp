6040a0074c617ad2dd0a6e5fefbdfc1432549fda
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-03-06 22:27:54 +0530

    session 18 repeatithon

---
+++ REPEATITHON/repeat.cpp
@@ -0,0 +1,701 @@
+======== 06 MARCH 2025 =========================================================================================================================================================
+
+#include <iostream> 
+#include <cstring> 
+#include <vector> 
+
+using std::cout;    // object of class ostream | ostream and cout are in namespace std 
+using std::endl;    // endl is defined in std namespace 
+using std::string;  // string is a class defined in std namespace (header -> string) 
+using std::vector;  // vector is a 'class template' defined in std namespace (header -> vector) 
+
+class Date 
+{
+    private: 
+        int day, month, year; 
+    public: 
+        Date(int init_day, int init_month, int init_year) 
+        {
+            day = init_day; 
+            month = init_month; 
+            year = init_year; 
+        }
+
+        void show(void) 
+        {
+            cout << day << "/" << month << "/" << year << endl; 
+        }
+}; 
+
+void vector_demo(void); 
+
+int main(void) 
+{
+    vector_demo(); 
+    return 0; 
+}
+
+void vector_demo(void) 
+{
+    // vector of ints 
+    vector<int> ivec1; 
+    vector<int> ivec2{10, 20, 30, 40, 50}; 
+    vector<int> ivec3(ivec2); 
+    vector<int> ivec4(5); 
+    vector<int> ivec5(5, 100);  
+}
+
+vector<int> ivec1; 
+vector<int> ivec2{10, 20, 30, 40, 50}; 
+vector<int> ivec3(ivec2); 
+vector<int> ivec4(5); 
+vector<int> ivec5(4, 200); 
+
+vector<int> ivec1; 
+vector<int> ivec2{10, 20, 30, 40, 50}; 
+vector<int> ivec3(ivec2); 
+vector<int> ivec4(5); 
+vector<int> ivec5(5, 100); 
+
+vector<int> ivec1; 
+vector<int> ivec2{10, 20, 30, 40, 50}; 
+vector<int> ivec3(ivec2); 
+vector<int> ivec4(5); 
+vector<int> ivec5(5, 100); 
+
+vector<int> ivec1; 
+vector<int> ivec2{10, 20, 30, 40, 50}; 
+vector<int> ivec3(ivec2); 
+vector<int> ivec4(5); 
+vector<int> ivec5(5, 100); 
+
+vector vector vector vector vector vector vector vector vector vector vecot r vecot rvecrore vector vector vector vector vector vector vector vector vector vector vector vecator vector vector vector vvector vector vector 
+vector vector vector vector vector vector vector vector vector vector vecetor vector vector vector vector vector vecotr vecot vector vectoe vecotvector vector vector lvecot<int> 
+vvector<int> vectot<int> 
+vector<int> vector<iont?> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> 
+vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> 
+vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vetor<int> vector<int> vector<int> vector<int> 
+vector<int> vector<int> vecator<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> 
+vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> vector<int> 
+
+vector<float> vector<float> vector<float> vector<float vector<float> vector<float> vector<float> vector<float vetor<float> 
+vector<double> vector<double> vector<double> vector<double> vecot<deouble> vector<double> vector<double> vecotr<deouble> 
+vetor<Date> vector<Date> vector<Date> vector<Date> vector<Date> vector<Date> vectir<Date> vector<Date> vector<Date> vector<Date> 
+vector<char> vector<char> vector<char> vector<char> vector<char> vector<char> vector<char> vector<char> vector<char> vector<char> 
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+
+using std::endl;    // endl is in std namespace 
+using std::cout;    // cout is a object of class ostream | cuot and ostream are in std namespace 
+using std::string;  // string is a class (header -> string) 
+using std::vector;  // vector is a class template (header -> vector) 
+
+class Date 
+{
+    private: 
+        int day, month, year; 
+    public: 
+        Date(int init_day, int init_month, int init_year) 
+        {
+            day = init_day; 
+            month = init_month; 
+            year = init_year; 
+        }
+
+        void show() 
+        {
+            cout << day << "/" << month << "/" << year << endl; 
+        }
+}; 
+
+void vector_demo(void); 
+
+int main(void) 
+{
+    vector_demo(); 
+    return 0; 
+}
+
+void vector_demo(void) 
+{
+    vector<int> ivec1; 
+    vector<int> ivec2{10, 20, 30, 40, 50}; 
+    vector<int> ivec3(ivec2); 
+    vector<int> ivec4(5); 
+    vector<int> ivec5(5, 100); 
+}
+
+#include <iostream> 
+#include <vector> 
+#include <string> 
+
+using std::cout;    // cout is a object of class ostream | class ostream and its object cout are in std namesoace 
+using std::endl;    // endl is defined in std namesoace 
+using std::vector;  // vector is a class template defined in std namespace (header -> vector) 
+using std::string;  // string is a class defined in std namespace (geder -> string) 
+
+class Date 
+{
+    private: 
+        int day, month, year; 
+    public: 
+        Date(int init_day, int init_month, int init_year) 
+        {
+            day = init_day; 
+            month = init_month; 
+            year = init_year; 
+        }
+
+        void show() 
+        {
+            cout << day << "/" << month << "/" << year << endl; 
+        }
+}; 
+
+void vector_demo(void); 
+
+int main(void) 
+{
+    vector_demo(); 
+    return 0; 
+}
+
+void vector_demo(void) 
+{
+    vector<int> ivec1; 
+    vector<int> ivec2{10, 20, 30, 40, 50}; 
+    vector<int> ivec3(ivec2); 
+    vector<int> ivec4(5); 
+    vector<int> ivec5(5, 100); 
+}
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+
+using std::cout;    // cout is a object of class ostream | class ostream and its object cout are in std namespace 
+using std::endl;    // endl is defined in std namespace 
+using std::string;  // string is a class defined in std namespace (header -> string) 
+using std::vector;  // vector is a class template defined in std namespace (header -> vector) 
+
+class Date 
+{
+    private: 
+        int day, month, year; 
+    public: 
+        Date(int init_day, int init_month, int init_year) 
+        {
+            day = init_day; 
+            month  = init_month; 
+            year = init_year; 
+        }
+
+        void show() 
+        {
+            cout << day << "/" << month << "/" << year << endl; 
+        }
+}; 
+
+void vector_demo(void); 
+
+int main(void) 
+{
+    vector_demo(); 
+    return 0; 
+}
+
+void vector_demo(void) 
+{
+    vector<int> ivec1; 
+    vector<int> ivec2{10, 20, 30, 40, 50}; 
+    vector<int> vec3(ivec2); 
+    vector<int> ivec4(5); 
+    vector<int> ivec5(5, 100); 
+}
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+
+using std::cout;    // cout is a object of class ostream | object cout and class ostream are defined in std namespace 
+using std::endl;    // endl is defined in std namespace 
+using std::string;  // string is a class defined in std namespace (heder -> string) 
+using std::vector;  // vector is a class template defined in std namespace (header -> vector) 
+
+class Date 
+{
+    private: 
+        int day, month, year; 
+    public: 
+        Date(int init_day, int init_month, int init_year) 
+        {
+            day = init_day; 
+            month = init_month; 
+            year = init_year; 
+        } 
+
+        void show() 
+        {
+            cout << day << "/" << month << "/" << year << endl; 
+        }
+}; 
+
+void vector_demo(void); 
+
+int main(void) 
+{
+    vector_demo(); 
+    return 0; 
+}
+
+void vector_demo(void) 
+{
+    vector<int> ivec1; 
+    vector<int> ivec2{10, 20, 30, 40, 50}; 
+    vector<int> ivec3(ivec2); 
+    vector<int> ivec4(5); 
+    vector<int> ivec5(5, 100); 
+}
+
+//------------------------------------------------------------
+
+#include <iostream> 
+#include <string> 
+
+using std::cout; 
+using std::endl; 
+using std::string; 
+
+void string_demo(void); 
+void vector_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+    vector_demo(); 
+
+    return (0); 
+}
+
+void string_demo(void) 
+{
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+
+    cout << "--------STRING DEMO ENTER----------" << endl; 
+    cout << "s1: " << s1 << endl;   // empty 
+    cout << "s2: " << s2 << endl;   // hello 
+    cout << "s3: " << s3 << endl;   // hello 
+    cout << "addr(s2) : " << &s2 << endl; 
+    cout << "addr(s3) : " << &s3 << endl; 
+    cout << "Contents of s1 and s2 are same but the addressess are not " << endl    
+            << "this proves that s2 is a clone of s2" << endl; 
+
+    cout << "Show string char by char : " << endl; 
+    cout << "METHOD 1: Using index : " << endl; 
+
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "---------- METHOD 1 END ------------" << endl; 
+
+    cout << "METHOD 2 : Using iterator : " << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter)
+        cout << "*iter = " << *iter << endl;  
+    cout << "------ Method 2 End -----------" << endl; 
+    cout << "------ STRING DEMO LEAVE ----------" << endl; 
+}
+
+//  string -> dynamically resizable array of characters 
+
+void vector_demo() 
+{
+
+}
+
+namespace std 
+{
+    class string 
+    {
+        public : 
+            typedef unsigned long long size_type; 
+    }; 
+}; 
+
+#include <string> 
+
+int main(void) 
+{
+    std::string::size_type i; 
+}
+
+// with using std::string 
+#include <string> 
+using std::string; 
+
+int main(void) 
+{
+    for(string::size_type i) 
+}
+
+namespace std 
+{
+    class string 
+    {
+        public: 
+            typedef unsigned short size_type; 
+    }
+}
+
+s2.fun();  // std::string::fun(&s2); 
+
+namespace std 
+{
+    class string 
+    {
+        public: 
+            typedef unsigned long long size_type; 
+
+            size_type size() 
+            {
+                // return length of the string pointed by this  
+            }
+    }
+}
+
+#include <iostream> 
+#include <string> 
+
+using std::cout; 
+using std::endl; 
+using std::string; 
+
+void string_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+
+    return (0); 
+}
+
+void string_demo(void) 
+{   
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+    
+    cout << "-------- STRING DEMO BEGIN ----------" <<endl; 
+    cout << "s1: " << s1 << endl; 
+    cout << "s2: " << s2 << endl; 
+    cout << "s3: " << s3 << endl; 
+    cout << "addr(s2) : " << &s2 << endl; 
+    cout << "addr(s3) : " << &s3 << endl; 
+    cout << "Contents of s2 and s3 are same but the addressedd are differene " << endl 
+            << "This proves s3 is a clone of s2." << endl; 
+
+    cout << "Show a string char by char : " << endl; 
+    cout << "Method 1: using index" << endl; 
+
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "------------- METHOD 1 END ---------------" << endl; 
+
+    cout << "METHOD 2 : Using iterator " << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter) 
+        cout << "*iter = " << *iter << endl; 
+    cout << "-------- METHOD 2 END --------" << endl; 
+    cout << "---------- STRING DEMO LEAVE ----------" << endl; 
+}
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+ 
+using std::endl; 
+using std::cout; 
+using std::vector; 
+
+void string_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+
+    return (0); 
+}
+
+void string_demo(void) 
+{
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+
+    cout << "-------- STRING DEMO START ------" << endl; 
+    cout << "s1: " << s1 << endl; 
+    cout << "s2: " << s2 << endl; 
+    cout << "s3: " << s3 << endl; 
+    cout << "addr(s2) : " << &s2 << endl; 
+    cout << "addr(s3) : " << &s3 << endl; 
+    cout << "Address of s2 and s3 are different while contents are same" << endl 
+            << "This proves s3 is a clone of s2" << endl; 
+    
+    cout << "Showing string char by char : " << endl; 
+    cout << "METHOD 1: Using index : " << endl; 
+
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "------ METHOD 1 END" << endl; 
+
+    cout << "METHOD 2 : using iterator" << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter)
+        cout << "*iter = " << *iter << endl; 
+    cout << "Method 2 END" << endl; 
+    cout << "----------- STRING DEMO LEAVE -------------" << endl; 
+}
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+
+using std::cout; 
+using std::endl; 
+using std::vector; 
+
+void string_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+    return 0; 
+}
+
+void string_demo(void) 
+{
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+
+    cout << "-------- STRING DEMO ENTER -----------" << endl; 
+    cout << "s1: " << s1 << endl; 
+    cout << "s2: " << s2 << endl; 
+    cout << "s3: " << s3 << endl; 
+    cout << "addr(s2) : " << &s2 << endl ; 
+    cout << "addr(s3) : " << &s3 << endl; 
+
+    cout << "Addresses of s2 and s3 are different while values are same" << endl 
+            << "This proves s3 is a clone of s2" << endl; 
+
+    cout << "Showing a string char by char " << endl; 
+    cout << "METHOD 1 : using index" << endl; 
+
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "------- METHOD 1 END --------" << endl; 
+
+    cout << "METHOD 2: Using iterator : " << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter) 
+        cout << "*iter = " << *iter << endl; 
+    cout << "--------- METHOD 2 END ----------" << endl; 
+    cout << "--------- STRING DEMO LEAVE ----------" << endl << endl; 
+}
+
+#include <iostream> 
+#include <string> 
+
+using std::endl; 
+using std::cout; 
+using std::string; 
+
+void string_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+    return (0); 
+}
+
+void string_demo(void) 
+{
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+
+    cout << "------ STRING DEMO ENTER -----------" << endl; 
+    cout << "s1: " << s1 << endl; 
+    cout << "s2: " << s2 << endl; 
+    cout << "s3: " << s3 << endl; 
+    cout << "addr(s2) : " << &s2 << endl; 
+    cout << "addr(s3) : " << &s3 << endl; 
+    cout << "Contents of s2 and s3 are same but addressess are not" << endl 
+            << "This proves s3 is a clone of s2" << endl; 
+
+    cout << "Show a string char by char : " << endl; 
+    cout << "METHOD 1: Using Index" << endl; 
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "-------- METHOD 1 END -------" << endl; 
+
+    cout << "METHOD 2 : Using iterator" << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter) 
+        cout << "*iter = " << *iter << endl; 
+    cout << "--- METHOD 2 END -------" << endl; 
+    cout << "----------- STRING DEMO LEAVE ------------" << endl; 
+}   
+
+#include <iostream> 
+#include <string> 
+
+using std::endl; 
+using std::cout; 
+using std::string; 
+
+void string_demo(void); 
+
+int main(void) 
+{
+    string_demo(); 
+    return 0; 
+}
+
+void string_demo(void) 
+{
+    string s1; 
+    string s2("Hello"); 
+    string s3(s2); 
+
+    cout << "--------------------- STRING DEMO ENTER --------------" << endl; 
+    cout << "s1: " << s1 << endl; 
+    cout << "s2: " << s2 << endl; 
+    cout << "s3: " << s3 << endl; 
+    cout << "addr(s2) : " << &s2 << endl; 
+    cout << "addr(s3) : " << &s3 << endl; 
+    cout << "contents of s2 and s3 are same but addressess are not" << endl 
+            << "This proves that s3 is a clone of s2" << endl; 
+
+    cout << "show a string char by char : " << endl; 
+    cout << "METHOD 1 : Using index" << endl; 
+
+    for(string::size_type i = 0; i != s2.size(); ++i) 
+        cout << "s2[" << i << "] = " << s2[i] << endl; 
+    cout << "----------- METHOD 1 END ----------" << endl; 
+    
+    cout << "METHOD 2 : Using iterator " << endl; 
+    for(string::iterator iter = s2.begin(); iter != s2.end(); ++iter) 
+        cout << "*iter = " << *iter << endl; 
+    cout << "---------- METHOD 2 END ----------" << endl; 
+    cout << "--------------------------- STRING DEMO LEAVE -------------" << endl; 
+}
+
+int p; 
+
+int main(void) 
+{
+    int i;  // definition point 
+
+    for() 
+    {
+        // i in main() is accessible
+        for() 
+        {
+            int k; 
+            // i in main() is accessible 
+            // k is accessible 
+        }
+        // k is not accessble here 
+    }
+    // k is not accessible here 
+}
+
+void f(void) 
+{
+    // i in main() is not accessible 
+}
+
+class Date 
+{
+    public: 
+        typedef int day_t; 
+
+        static void test() 
+        {
+
+        }
+
+        class XYZ 
+        {} 
+}
+
+#include <iostream> 
+
+using std::cout; 
+using std::endl; 
+
+namespace X 
+{
+    // data variable inside namespace 
+    int num; 
+
+    // function variable inside namespace 
+    void test() 
+    {
+        cout << "Hello world" << endl; 
+    }
+
+    // class inside namespace 
+    class Date 
+    {
+        private: 
+            int day, month, year; 
+        public: 
+            Date() 
+            {
+                day = 1; 
+                month = 1; 
+                year = 1970; 
+            }
+    }; 
+
+    // typedef inside namespace 
+    typedef int xyz_t; 
+
+    // namesoace inside namespace 
+    namespace Y 
+    {
+        int p; 
+    } 
+}; 
+
+class Test 
+{
+    public: 
+        int num;            // data member  
+        static int snum;    // static data member 
+
+        // member function 
+        void test_function() 
+        {
+            cout << "Inside test()" << endl; 
+        }
+
+        // statuc member function 
+        static void stest_function() 
+        {
+            cout << "Inside static test() " << endl; 
+        }
+
+        // typedef 
+        typedef int pqr_t; 
+
+        // class 
+        class PQR 
+        {
+            private: 
+                int p, q, r; 
+        }
+}; 
+
+// definition of static variable snum inside class Test 
+int Test::snum = 100; 
