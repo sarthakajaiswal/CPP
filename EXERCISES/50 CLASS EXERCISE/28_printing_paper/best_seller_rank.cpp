5b3497c629379f63ab09b09b1b658483bc58710c
Sarthak Jaiswal <sarthakajaiswal@gmail.com>
2025-03-16 12:25:24 +0530

    deletes last batch exercise

--- EXERCISES/50 CLASS EXERCISE/28_printing_paper/best_seller_rank.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-#include "best_seller_rank.hpp" 
-
-BestSellerRank::BestSellerRank(unsigned int rank, std::string category) : m_rank(rank), m_category(category) {} 
-
-std::ostream& operator<<(std::ostream& os, const BestSellerRank& obj) {
-    os << "#" << obj.m_rank << " in " << obj.m_category; 
-    return os; 
-}
--- EXERCISES/50 CLASS EXERCISE/28_printing_paper/test.bat
+++
@@ -1,5 +0,0 @@
-cl /Fe:app.exe address.cpp /EHsc
-cl /Fe:app.exe best_seller_rank.cpp /EHsc
-cl /Fe:app.exe count.cpp /EHsc
-cl /Fe:app.exe dimention.cpp length.cpp /EHsc
-cl /Fe:app.exe length.cpp /EHsc
